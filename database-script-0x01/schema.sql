611e4fe262eb94f19db34003b0213a8302070a74
SIDEEQ HAMMED <sideeqhammed05@gmail.com>
2025-05-12 11:12:09 +0100

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,1 +1,12 @@
 
+--- create user table
+CREATE TABLE User (
+  user_id: Primary Key, UUID, Indexed,
+  first_name: VARCHAR, NOT NULL,
+  last_name: VARCHAR, NOT NULL,
+  email: VARCHAR, UNIQUE, NOT NULL,
+  password_hash: VARCHAR, NOT NULL,
+  phone_number: VARCHAR, NULL,
+  role: ENUM (guest, host, admin), NOT NULL,
+  created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+);
