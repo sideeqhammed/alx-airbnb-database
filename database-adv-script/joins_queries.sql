07bcf566afb45b12f8bd828b5d40634a21c74e5b
SIDEEQ HAMMED <sideeqhammed05@gmail.com>
2025-05-14 07:48:35 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,9 @@
 -- retrieve all bookings and the respective users who made those bookings.
 SELECT booking.booking_id, user.user_id
 FROM booking
-INNER JOIN user
-ON booking.user_id = user.user_id
+INNER JOIN user ON booking.user_id = user.user_id
+
+-- retrieve all properties and their reviews, including properties that have no reviews.
+SELECT property.property_id, review.comment
+FROM property
+LEFT JOIN property ON property.property_id = review.property_id
