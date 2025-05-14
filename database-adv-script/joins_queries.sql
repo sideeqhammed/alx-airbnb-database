2f0e0362d54f63e54105518f98a835b064d22ffa
SIDEEQ HAMMED <sideeqhammed05@gmail.com>
2025-05-14 07:53:13 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,3 +7,8 @@ INNER JOIN user ON booking.user_id = user.user_id
 SELECT property.property_id, review.comment
 FROM property
 LEFT JOIN property ON property.property_id = review.property_id
+
+-- retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+SELECT user.user_id, booking.booking_id
+FROM user
+FULL OUTER JOIN booking ON user.user_id = booking.user_id
