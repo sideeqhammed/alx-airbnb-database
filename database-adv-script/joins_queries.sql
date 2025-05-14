1dfbedfbbd2e7b3d92748986026962d1cb7fe865
SIDEEQ HAMMED <sideeqhammed05@gmail.com>
2025-05-14 07:39:35 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,5 @@
-
+-- retrieve all bookings and the respective users who made those bookings.
+SELECT booking.booking_id, user.user_id
+FROM booking
+INNER JOIN user
+ON booking.user_id = user.user_id
