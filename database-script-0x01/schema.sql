93ae85bae9cf82fd1d50a7fba32ad8e80a7b0bc8
SIDEEQ HAMMED <sideeqhammed05@gmail.com>
2025-05-15 16:11:50 +0100

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,44 +1,44 @@
 
 --- create user table
-CREATE TABLE User (
-  user_id: Primary Key, UUID, Indexed,
-  first_name: VARCHAR, NOT NULL,
-  last_name: VARCHAR, NOT NULL,
-  email: VARCHAR, UNIQUE, NOT NULL,
-  password_hash: VARCHAR, NOT NULL,
-  phone_number: VARCHAR, NULL,
-  role: ENUM (guest, host, admin), NOT NULL,
-  created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE user (
+  user_id VARCHAR(5) Primary KEY,
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  email VARCHAR(30) UNIQUE NOT NULL,
+  password_hash VARCHAR(20) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  role VARCHAR(10) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 --- create property table
-CREATE TABLE Property (
-  property_id: Primary Key, UUID, Indexed,
-  host_id: Foreign Key, references User(user_id),
-  name: VARCHAR, NOT NULL,
-  description: TEXT, NOT NULL,
-  location: VARCHAR, NOT NULL,
-  pricepernight: DECIMAL, NOT NULL,
-  created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP,
-  updated_at: TIMESTAMP, ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (host_id) REFERENCES User(user_id)
+CREATE TABLE property (
+  property_id VARCHAR(5) Primary Key,
+  host_id VARCHAR(5) NOT NULL,
+  name VARCHAR(20) NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  location VARCHAR(20) NOT NULL,
+  pricepernight DECIMAL NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (host_id) REFERENCES user(user_id)
 );
 
 -- Create an index on the host_id for faster queries
 CREATE INDEX idx_host_id ON Property (host_id);
 
 --- create booking table
-CREATE TABLE Booking (
-  booking_id: Primary Key, UUID, Indexed,
-  property_id: Foreign Key, references Property(property_id),
-  user_id: Foreign Key, references User(user_id),
-  start_date: DATE, NOT NULL,
-  end_date: DATE, NOT NULL,
-  total_price: DECIMAL, NOT NULL,
-  status: ENUM (pending, confirmed, canceled), NOT NULL,
-  created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (property_id) REFERENCES Property(property_id),
-  FOREIGN KEY (user_id) REFERENCES User(User_id)
+CREATE TABLE booking (
+  booking_id VARCHAR(5) NOT NULL Primary Key,
+  property_id VARCHAR(5) NOT NULL,
+  user_id VARCHAR(5) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  total_price DECIMAL NOT NULL,
+  status VARCHAR(10) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (property_id) REFERENCES property(property_id),
+  FOREIGN KEY (user_id) REFERENCES user(User_id)
 );
 
 -- Create indexes for property_id and user_id for optimized query performance
@@ -46,26 +46,25 @@ CREATE INDEX idx_property_id ON Booking(property_id);
 CREATE INDEX idx_user_id ON Booking(user_id);
 
 --- create payment table
-CREATE TABLE Payment (
-	payment_id: Primary Key, UUID, Indexed,
-	booking_id: Foreign Key, references Booking(booking_id),
-	amount: DECIMAL, NOT NULL,
-	payment_date: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP,
-	payment_method: ENUM (credit_card, paypal, stripe), NOT NULL,
-	FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+CREATE TABLE payment (
+	payment_id VARCHAR(5) NOT NULL Primary Key,
+	booking_id VARCHAR(5) NOT NULL,
+	amount DECIMAL NOT NULL,
+	payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	payment_method VARCHAR(5) NOT NULL,
+	FOREIGN KEY (booking_id) REFERENCES booking(booking_id)
 );
-
 -- Create an index on booking_id for optimized query performance
 CREATE INDEX idx_booking_id ON Payment(booking_id);
 
 --- create review table
-CREATE TABLE Review (
-	review_id: Primary Key, UUID, Indexed,
-	property_id: Foreign Key, references Property(property_id),
-	user_id: Foreign Key, references User(user_id,
-	rating: INTEGER, CHECK: rating >= 1 AND rating <= 5, NOT NULL,
-	comment: TEXT, NOT NULL,
-	created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE review (
+	review_id VARCHAR(5) NOT NULL Primary Key,
+	property_id VARCHAR(5) NOT NULL,
+	user_id VARCHAR(5) NOT NULL,
+	rating INT NOT NULL,
+	comment TEXT NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (property_id) REFERENCES Property(property_id)
 );
 
